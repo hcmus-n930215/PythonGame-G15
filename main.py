fc094e9929d7db091dd96871f19821b59b831220
viplazylmht <viplazylmht@gmail.com>
2019-03-22 15:37:52 +0700

    Divider funtions and class to libs/gobal_variables.py
    Signed-off-by: viplazylmht <viplazylmht@gmail.com>

--- main.py
+++ main.py
@@ -1,7 +1,41 @@
-from libs.gobal_variables import GET_GOBAL_VAL
+from libs.gobal_variables import INIT_GAME , Racer, Amulet, GameController, Player
+import pygame
+import time
+import math
+import random
 
-gb = GET_GOBAL_VAL()
+gameLancher = INIT_GAME()
 
-print(gb.DELAY_TIME)
+gameLancher.draw_map()
+
+def data_manager():
+    """Xu li file, du lieu"""
 
 
+# main code
+player = Player()
+
+racers = (Racer(0, 170+0*50,gameLancher), Racer(0, 170+1*50,gameLancher), Racer(0, 170+2*50,gameLancher),
+          Racer(0, 170+3*50,gameLancher), Racer(0, 170+4*50,gameLancher), Racer(0, 170+5*50,gameLancher))
+
+finish = False
+
+
+
+while not finish:
+
+    gameLancher.draw_map()
+    for r in racers:
+        r.update()
+        r.draw()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            finish = True
+
+    pygame.display.update()
+    gameLancher.clock.tick(gameLancher.FPS)
+
+
+pygame.quit()
+exit()
