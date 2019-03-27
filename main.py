a3e4628532f7168fe707fe3d26be735125126883
viplazylmht <viplazylmht@gmail.com>
2019-03-28 00:51:45 +0700

    Update version 1.2
    Signed-off-by: viplazylmht <viplazylmht@gmail.com>

--- main.py
+++ main.py
@@ -1,27 +1,69 @@
 from libs.global_variables import *
-from libs.Widgets import *
+#from libs.Widgets import *
+from libs.LoginPage import *
 import pygame
 import time
 import time
 import math
 import random
 
-def main():
+
+
+
+
+
+def loginActivity():
+    gameLancher.draw_map(2)
+    loginPage = LoginPage(gameLancher)
+    loginPage.drawLoginPage()
+    btn_signin = loginPage.btn_signin
+    pygame.display.flip()
+    input_boxes = [loginPage.userNameInput, loginPage.passWordInput]
+
+    while True:
+        if btn_signin.is_clicked():
+            btn_signin.setText("XIN CHAO")
+            pygame.display.update(btn_signin.rect)
+            time.sleep(0.08)
+            gameLancher.IS_SIGNED_IN = True
+            return
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit(0)
+            for box in input_boxes:
+                box.handle_event(event)
+        loginPage.drawLoginPage()
+        for box in input_boxes:
+            box.update()
+
+
+        #gameLancher.SCREEN.fill((30, 30, 30))
+        for box in input_boxes:
+            box.draw(gameLancher.SCREEN)
+
+        pygame.display.flip()
+
+
+def main_game():
     timenow = int(round(time.time() * 1000))
     lasttime = timenow
+
     gameLancher.draw_map(0)
 
     # main code
     player = Player()
 
-    racers = (Racer(0, 170 + 0 * 50, gameLancher, "ic_snail", 0),
-              Racer(0, 170 + 1 * 50, gameLancher, "ic_snail", 1),
-              Racer(0, 170 + 2 * 50, gameLancher, "ic_snail", 2),
-              Racer(0, 170 + 3 * 50, gameLancher, "ic_snail", 3),
-              Racer(0, 170 + 4 * 50, gameLancher, "ic_snail", 4),
-              Racer(0, 170 + 5 * 50, gameLancher, "ic_snail", 5))
+    racers = (Racer(350, 170 + 0 * 50, gameLancher, "ic_snail", 0),
+              Racer(350, 170 + 1 * 50, gameLancher, "ic_snail", 1),
+              Racer(350, 170 + 2 * 50, gameLancher, "ic_snail", 2),
+              Racer(350, 170 + 3 * 50, gameLancher, "ic_snail", 3),
+              Racer(350, 170 + 4 * 50, gameLancher, "ic_snail", 4),
+              Racer(350, 170 + 5 * 50, gameLancher, "ic_snail", 5))
 
     ranking = Ranking(gameLancher, racers)
+    camera = Camera(gameLancher)
+
     finish = False
 
     btn = Button(400, 200, 100, 100, "START")
@@ -39,32 +81,37 @@ def main():
     gameLancher.draw_map(0)
 
     pygame.display.flip()
-    time_count = 0
+    time_count = 2001
+    max_speed = 0
+    isScrolling = True
     while not finish:
         list_area_to_be_update_display = []
         # gameLancher.SCREEN.fill(180)
         ###########################
         timenow = int(round(time.time() * 1000))
 
-        if timenow-lasttime> gameLancher.TIME_INTERVAL:
+        if timenow - lasttime > gameLancher.TIME_INTERVAL:
             # Xu ly giao dien sau moi Frame (FPS) (draw lai giao dien o dong #57-58
-            time_count+=gameLancher.TIME_INTERVAL
-            if time_count > 2000:
+            time_count += gameLancher.TIME_INTERVAL
+            if time_count > 1000:
                 # Xu ly su kien sau moi 2 giay (thay doi toc do racer)
-                time_count-=2000
+                time_count -= 1000
+
                 for r in racers:
                     r.updatespeed()
+
             if play:
-                rollback -= gameLancher.ROLLBACK_STEP
+                # rollback -= gameLancher.ROLLBACK_STEP
+                rollback += camera.delta
             gameLancher.draw_map(rollback)
 
         ###########################
+
         btn.show()
         x, y = pygame.mouse.get_pos()
         tv_cusor_X.setText("CUSOR X: " + str(x))
         tv_cusor_Y.setText("CUSOR Y: " + str(y))
-
-        tv_data.setText(str(gameLancher.SCREEN.get_clip()) + " == ")
+        tv_data.setText(str(gameLancher.SCREEN.get_clip()))
 
         if btn.is_clicked():
             if not isPressed:
@@ -81,24 +128,47 @@ def main():
             isPressed = False
 
         if tv_cusor_X.is_clicked():
-            tv_cusor_X.setText()
+            tv_cusor_X.setText("XIN CHAO")
             time.sleep(0.08)
-        if play:
+        max_speed = 0
+        max_racer = 0
+        if isScrolling:
             for r in racers:
-                r.update()
+                if r.x > max_racer:
+                    max_speed = r.speed
+                    max_racer = r.x
+        for r in racers:
+            if play:
+                if not r.update(camera):
+                    max_speed = 0
+                    isScrolling = False
 
-                list_area_to_be_update_display.append(r.clear())
-                list_area_to_be_update_display.append(r.draw())
+            # list_area_to_be_update_display.append(r.clear())
+            list_area_to_be_update_display.append(r.draw())
+        print("")
+        ranking.update(racers)
+        camera.update(racers[camera.follow])
 
-            ranking.update(racers)
+        if play:
+            gameLancher.DISTANCE += camera.delta
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 finish = True
 
         pygame.display.flip()
+
         # pygame.display.update(list_area_to_be_update_display)
         # time.sleep(1) # sleep 1 sec
+
+def main():
+    if not gameLancher.IS_SIGNED_IN:
+        loginActivity()
+        time.sleep(1)
+    main_game()
+
+
+
 gameLancher = INIT_GAME()
 main()
 pygame.quit()
