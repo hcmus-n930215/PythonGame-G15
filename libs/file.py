c4229ba79a8015726752c0f0f5c4c60992aedb27
BHD233 <32787292+BHD233@users.noreply.github.com>
2019-03-27 17:32:55 +0700

    Add files via upload

---
+++ libs/file.py
@@ -0,0 +1,191 @@
+from libs.global_variables import *
+from libs.decryptData import *
+from libs.encryptdata import *
+
+import os
+
+#key to encript and decrip      from 1 to 127
+key = 125
+
+userIDTitle = "ID = \""
+nameTitle = "username = \""
+passTitle = "password = \""
+coinsTitle = "coins = \""
+playTimeTitle = "playTime = \""
+winrateTitle = "winrate = \""
+
+direct = os.getcwd()
+currentDirrect = "/libs"
+dataDirect = direct[0]
+
+#change direction
+i = 1
+while(i < len(direct) - len(currentDirrect)):
+    dataDirect = dataDirect + direct[i]
+    i = i + 1
+
+os.chdir(dataDirect)
+
+aid = os.getcwd()
+
+print(dataDirect)
+
+endOfData = "\""
+
+#create líst
+#user = ["buiHuu Duc"]
+#password = ["456"]
+#coins = ["123"]
+#playTime = ["789"]
+#winrate =["100"]
+
+user = []
+
+class ReadUsersData:
+
+    def StringCompare(a, b, startPossOfA):
+        i = startPossOfA
+        j = 0
+        x= len(a)
+        y= len(b)
+        while((i < len(a)) & (j < len(b))):
+            if(a[i] != b[j]):
+                #c = a[i]
+                #d = a[i+1]
+                #u = a[i-1]
+                return False
+            i = i + 1
+            j = j + 1
+        return True
+
+    def StringCopy(src, startPoss):
+        if(startPoss < len(src) - 1):
+            dest = src[startPoss]
+
+            i = startPoss + 1
+            while(src[i] != endOfData):
+                dest = dest + src[i]
+                i = i + 1
+            return dest
+
+    def GetUserData(data, startPoss):
+        i = startPoss
+
+        # get ID
+        if (ReadUsersData.StringCompare(data, userIDTitle, i)):
+            i = i + len(userIDTitle)
+            user.append(User())
+            user[len(user) - 1].ID = ReadUsersData.StringCopy(data, i)
+            a = user[0].ID
+            print(user[0].ID)
+        else:
+            return -1
+
+        #get name
+        i = i + len(user[len(user) - 1].name) + 2
+        if (ReadUsersData.StringCompare(data, nameTitle, i)):
+            i = i + len(nameTitle)
+            user[len(user) - 1].name = ReadUsersData.StringCopy(data, i)
+            a = user[0].name
+            print(user[0].name)
+        else:
+            return -1
+
+        #get pass
+        i = i + len(user[len(user) - 1].name) + 2    #change poss to pass....
+        if (ReadUsersData.StringCompare(data, passTitle, i)):
+            i = i + len((passTitle))
+            user[len(user) - 1].password = ReadUsersData.StringCopy(data, i)
+            a = user[0].password
+            print(user[0].password)
+        else:
+            return -1
+
+        #get coins
+        i = i + len(user[len(user) - 1].password) + 2    #change poss to coins....
+        if (ReadUsersData.StringCompare(data, coinsTitle, i)):
+            i = i + len((coinsTitle))
+            user[len(user) - 1].coins = ReadUsersData.StringCopy(data, i)
+            a = user[0].coins
+            print(user[0].coins)
+        else:
+            return -1
+
+        #get playtime
+        i = i + len(str(user[len(user) - 1].coins)) + 2    #change poss to playtime....
+        if (ReadUsersData.StringCompare(data, playTimeTitle, i)):
+            i = i + len((playTimeTitle))
+            user[len(user) - 1].playTime = ReadUsersData.StringCopy(data, i)
+            a = user[0].playTime
+            print(user[0].playTime)
+        else:
+            return -1
+
+        # get winrate
+        i = i + len(str(user[len(user) - 1].playTime)) + 2 # change poss to winrate....
+        if (ReadUsersData.StringCompare(data, winrateTitle, i)):
+            i = i + len((winrateTitle))
+            user[len(user) - 1].winrate = ReadUsersData.StringCopy(data, i)
+            a = user[0].winrate
+            print(user[0].winrate)
+        else:
+            return -1
+
+        i = i + len(str(user[len(user) - 1].winrate)) + 2    #change poss to end of user
+        return i
+
+    #ham tra ve -1 neu khong doc duoc
+    def  GetAllUsersData():
+
+        f = open("data/usersData.txt", "rt")
+
+        data = f.read()
+
+        data = decrypt.DecyptData(data, key)
+
+        dataLength = len(data)
+        i = 0
+        while(i < dataLength - 20):
+            i = ReadUsersData.GetUserData(data,i)
+            if(i == -1):
+                print("loi")
+                return -1
+
+    pass
+
+class WriteUsersData:
+
+    def WriteAllUsersData():
+        f = open("data/usersData.txt", "w")
+
+        i = 0
+        textToWrite = ""
+        while(i < len(user)):
+            textToWrite += userIDTitle + user[i].ID + "\" "
+
+            textToWrite += nameTitle + user[i].name + "\" "
+
+            textToWrite += passTitle + user[i].password + "\" "
+
+            textToWrite += coinsTitle + user[i].coins + "\" "
+
+            textToWrite += playTimeTitle + user[i].playTime + "\" "
+
+            textToWrite += winrateTitle + user[i].winrate + "\" "
+
+            #textToWrite = textToWrite + '\n'
+
+            i = i + 1
+
+        textToWrite = encypt.EncryptData(textToWrite, key)
+
+        f.write(textToWrite)
+
+    pass
+
+if(ReadUsersData.GetAllUsersData() != -1):
+
+    WriteUsersData.WriteAllUsersData()
+
+
+
--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-from script.global_variables import INIT_GAME, Amulet, GameController, Player
-from script.main_class import Racer
-
+from libs.global_variables import *
+from libs.Widgets import *
 import pygame
+import time
 import time
 import math
 import random
 
 gameLancher = INIT_GAME()
 
-gameLancher.draw_map()
+gameLancher.draw_map(0)
 
 def data_manager():
     """Xu li file, du lieu"""
@@ -17,27 +17,85 @@ def data_manager():
 # main code
 player = Player()
 
-racers = (Racer(0, 170+0*50,gameLancher.IC_CIRCLE), Racer(0, 170+1*50,gameLancher), Racer(0, 170+2*50,gameLancher),
-          Racer(0, 170+3*50,gameLancher), Racer(0, 170+4*50,gameLancher), Racer(0, 170+5*50,gameLancher))
 
+racers = (Racer(0, 170+0*50, gameLancher, "ic_snail", 0),
+          Racer(0, 170+1*50, gameLancher, "ic_snail", 1),
+          Racer(0, 170+2*50, gameLancher, "ic_snail", 2),
+          Racer(0, 170+3*50, gameLancher, "ic_snail", 3),
+          Racer(0, 170+4*50, gameLancher, "ic_snail", 4),
+          Racer(0, 170+5*50, gameLancher, "ic_snail", 5))
+
+ranking = Ranking(gameLancher, racers)
 finish = False
 
 
+btn = Button(400, 200, 100, 100, "START")
+
+tv_cusor_X = TextView(200,200,100,50,"CUSOR X: ")
+tv_cusor_Y = TextView(200,300,100,50,"CUSOR Y: ")
+tv_data = TextView(200,400,100,50)
+
+subScreen = btn.setSurface(gameLancher.SCREEN)
+
+play = False
+isPressed = False
+rollback = 0
+list_area_to_be_update_display = []
+gameLancher.draw_map(0)
+
+pygame.display.flip()
 
 while not finish:
+    #gameLancher.SCREEN.fill(180)
+    gameLancher.draw_map(rollback)
+    rollback-=gameLancher.ROLLBACK_STEP
 
-    gameLancher.draw_map()
-    for r in racers:
-        r.update()
-        r.draw()
+    list_area_to_be_update_display = []
+
+    btn.show()
+    x, y = pygame.mouse.get_pos()
+    tv_cusor_X.setText("CUSOR X: " + str(x))
+    tv_cusor_Y.setText("CUSOR Y: " + str(y))
+
+    tv_data.setText(str(gameLancher.SCREEN.get_clip()) + " == ")
+
+    if btn.is_clicked():
+        if not isPressed:
+            if play:
+
+                btn.setText("RESUME")
+                play = False
+            else:
+                btn.setText("STOP")
+                play = True
+                btn.stop()
+            isPressed = True
+    else:
+        isPressed = False
+
+    if tv_cusor_X.is_clicked():
+        tv_cusor_X.setText()
+        time.sleep(0.08)
+    if play:
+        for r in racers:
+            r.update()
+
+            list_area_to_be_update_display.append(r.clear())
+            list_area_to_be_update_display.append(r.draw())
+
+        ranking.update(racers)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finish = True
 
-    pygame.display.update()
-    gameLancher.clock.tick(gameLancher.FPS)
+
+
+    pygame.display.flip()
+    #pygame.display.update(list_area_to_be_update_display)
+    # time.sleep(1) # sleep 1 sec
 
 
 pygame.quit()
+del gameLancher
 exit()
